3e296c40671337b73b2332d917e2222e2bdcaa2d
Neha <nkeswani14@gmail.com>
2023-04-18 16:52:50 +0530

    Test push

---
+++ mongotest.py
@@ -0,0 +1,21 @@
+from pymongo.mongo_client import MongoClient
+uri = "mongodb+srv://nehakeswani:Tomandjerry@cluster0.mdmqekb.mongodb.net/?retryWrites=true&w=majority"
+# Create a new client and connect to the server
+client = MongoClient(uri)
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+
+d = {
+    "name" : "neha",
+    "surname" : "keswani",
+    "email" : "nkeswani14"
+}
+
+db = client['mongotest']
+coll = db['test']
+coll.insert_one(d)
